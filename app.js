0b33964efdf4f21d9795ff10cf3107f2d62157b5
iamhieuxz <thehieucntt9x@gmail.com>
2019-05-28 05:15:13 +0700

    commit lan 2 , gan xong

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+// overlay
+
+  TweenMax.to('.overlay h1',2 , {
+    opacity : 0,
+    y : -60,
+    ease : Expo.easeInOut
+  })
+  TweenMax.to('.overlay',2 , {
+    delay : 1,
+    top : "-100%",
+    ease : Expo.easeInOut
+  })
+
 // navSilde animation 
 const navSlide = ()=> {
      const burger = document.querySelector('.burger');
